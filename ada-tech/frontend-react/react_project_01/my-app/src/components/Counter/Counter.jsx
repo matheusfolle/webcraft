fb1b041cd2e306b86b2678a554d7ec7f6f0856d9
Matheus Folle <mathfolle@icloud.com>
2025-07-12 15:00:16 -0300

    fix: re-add my-app as a regular folder after removing submodule

---
+++ ada-tech/frontend-react/react_project_01/my-app/src/components/Counter/Counter.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export class Counter extends React.Component {
+    render() {
+        return (
+        <div>
+        <h1>0</h1>
+        <div>
+            <button>Diminuir</button>
+            <button>Aumentar</button>
+        </div>
+        </div>
+        )
+    }
+}
